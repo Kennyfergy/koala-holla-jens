178dc0d9fbe1bb25ded1c86854aa5ac4214878e9
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-15 11:45:55 -0500

    Get request in router

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const router = express.Router();
 const koalaRouter = express.Router();
-
 // DB CONNECTION
 const pool = require("../modules/pool");
 
 // GET
+router.get("/", (req, res) => {
+  console.log("GET request at /koala");
+  const queryText = `SELECT * FROM "koala_database" ORDER BY "id" ASC;`;
+
+  pool
+    .query(queryText)
+    .then((response) => res.send(response.rows))
+    .catch((err) => {
+      console.log("Error in GET request", err);
+
+      res.sendStatus(500);
+    });
+});
 
 // POST
 
