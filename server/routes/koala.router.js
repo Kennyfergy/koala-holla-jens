0758e04a08b1a147670876042a7c100b23cc0616
Said Said <ssaid20@icloud.com>
2023-09-15 13:01:01 -0500

    route for put

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const koalaRouter = express.Router();
-
+// const koalaRouter = express.Router();
+const router = express.Router();
 // DB CONNECTION
 const pool = require("../modules/pool");
 
@@ -9,7 +9,19 @@ const pool = require("../modules/pool");
 // POST
 
 // PUT
-
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const ready_to_transfer = req.body.ready_to_transfer;
+    
+    const queryText = `UPDATE "profile" SET "ready_to_transfer" = $1 WHERE "id" = $2;`;
+  
+    pool.query(queryText, [ready_to_transfer, id])
+      .then(() => res.sendStatus(204))
+      .catch((err) => {
+        console.log("Error in UPDATING profile table", err);
+        res.sendStatus(500);
+      });
+  });
 // DELETE
 
-module.exports = koalaRouter;
+module.exports = router;
